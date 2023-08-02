b2f0202bbd69da11034b2f78e06f0247e1b498f8
Maria <mariahdude89@gmail.com>
2023-08-02 19:52:03 -0300

    Meu primeiro shell script

---
+++ primeiro.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Meu primeiro script com bash
+
+echo 'Acabei tendo uma reação com a vacina do covid e fiquei ruim por uns dias'
+echo 'Mas gostaria de falar que queria muito poder fazer parte da empresa'
+echo 'Admiro o trabalho desenvolvido pela empresa e o compromisso com a segurança de dados'
+echo 'Estou sempre em busca do aprendizado'
+echo 'Determinada a dar o meu melhor e poder contribuir ao máximo'
+exit 0
